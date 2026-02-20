be21f000e1d454d4c118ea68ddf9b8efe82c51f7
Prit Patel <pritpatel12075@gmail.com>
2026-02-20 05:47:08 +0530

    Generate HTML for order summary

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,6 @@
-import {cart} from '../data/cart.js';
+import {cart, addtoCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+
 let productsHTML = '';
 
 products.forEach((products) => {
@@ -57,37 +59,19 @@ products.forEach((products) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+function updateCartQuantity() {
+    let cartquantity = 0;
+    cart.forEach((item) => {
+        cartquantity += Number(item.quantity || 0);
+    });
+    document.querySelector('.js-cart-qty').innerHTML = cartquantity;
+}
+
 document.querySelectorAll('.js-add-to-cart')
  .forEach((button) => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
-
-        let matchingItem;
-        cart.forEach((item) => {
-            if (productId === item.productId) {
-                matchingItem = item;
-            }
-        });
-        if(matchingItem) {
-            matchingItem.quantity += 1;
-        } else {
-            cart.push({
-                productId: productId,
-                quantity: 1
-            });
-        }
-
-        let cartquantity = 0;
-
-        cart.forEach((item) => {
-            cartquantity += Number(item.quantity || 0);
-        });
-
-        console.log(cartquantity);
-
-
-        console.log(cart)
-
-        document.querySelector('.js-cart-qty').innerHTML = cartquantity
+        addtoCart(productId);
+        updateCartQuantity();
     });
  });
